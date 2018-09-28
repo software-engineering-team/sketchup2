dce364d6a0a9e830a1fbd0ba3554a12d5a9a784c
Yxiaoyao <43399315+Yxiaoyao@users.noreply.github.com>
2018-09-28 17:23:02 +0800

    Update AssemblyInfo.cs

--- Sketch2Code/Sketch2Code.AI/Properties/AssemblyInfo.cs
+++ Sketch2Code/Sketch2Code.AI/Properties/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+//通过以下属性集来控制程序集的一般信息。更改这些属性值以修改与程序集关联的信息。
 [assembly: AssemblyTitle("Sketch2Code.AI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -17,12 +18,15 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
+//将ComVisible设置为FALSE会使COM组件中的类型不可见。如果需要从COM中访问此程序集中的类型，请将该可编辑属性设置为True。
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+//下面的指南为类型库的ID，如果这个项目是向COM公开
 [assembly: Guid("47bce6ca-9347-4fb6-900b-770426958083")]
 
 // Version information for an assembly consists of the following four values:
+//程序集的版本信息由以下四个值组成：
 //
 //      Major Version
 //      Minor Version
@@ -31,6 +35,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
+//您可以指定所有的值，或者可以使用如下所示的“*”来默认生成和修订号：
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
